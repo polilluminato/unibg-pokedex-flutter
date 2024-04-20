c3826ed3bb431c11c934a7548a30fc540c944ebc
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-04-20 12:59:19 +0200

    feat: Add List and Grid of Pokémon

--- lib/pages/home/tabs/grid_tab.dart
+++ lib/pages/home/tabs/grid_tab.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:unibg_pokemon/models/pokemon_item.dart';
+import 'package:unibg_pokemon/pages/singlepokemon_page.dart';
+import 'package:unibg_pokemon/repository/pokemon_repository.dart';
 
 class GridTab extends StatelessWidget {
   const GridTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Grid"),);
+    return FutureBuilder<List<PokemonItem>?>(
+      future: PokemonRepository().getPokemonList(),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3,
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+              ),
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+
+                PokemonItem thisPokemon = snapshot.data![index];
+
+                return ListTile(
+                  title: Text(thisPokemon.name),
+                  leading: Image.network(
+                    thisPokemon.imageLink!,
+                    width: 50,
+                  ),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SinglePokemonPage(
+                        pokemonId: thisPokemon.id!,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: Text("Errore"),
+            );
+          }
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
--- lib/pages/home/tabs/list_tab.dart
+++ lib/pages/home/tabs/list_tab.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:unibg_pokemon/models/pokemon_item.dart';
+import 'package:unibg_pokemon/pages/singlepokemon_page.dart';
+import 'package:unibg_pokemon/repository/pokemon_repository.dart';
 
 class ListTab extends StatelessWidget {
   const ListTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("List"),);
+    return FutureBuilder<List<PokemonItem>?>(
+      future: PokemonRepository().getPokemonList(),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+
+                PokemonItem thisPokemon = snapshot.data![index];
+
+                return ListTile(
+                  title: Text(thisPokemon.name),
+                  leading: Image.network(
+                    thisPokemon.imageLink!,
+                    width: 50,
+                  ),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SinglePokemonPage(
+                        pokemonId: thisPokemon.id!,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: Text("Errore"),
+            );
+          }
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
