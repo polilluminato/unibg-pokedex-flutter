d6ad356fb5c53031ea659f59c25d17ef3f54b035
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-04-30 23:14:14 +0200

    feat: Add regex to get the id of the Pokèmon from the url

--- lib/models/pokemon_item.dart
+++ lib/models/pokemon_item.dart
@@ -18,9 +18,12 @@ class PokemonItem {
     //  lettera maiuscola: bulbasaur -> Bulbasaur
     String name =
         mameFromApi[0].toUpperCase() + mameFromApi.substring(1).toLowerCase();
-    //Link fatto come https://pokeapi.co/api/v2/pokemon/5/
-    //  vado a prenderne id alla fine
-    int id = int.parse(url.substring(url.length - 2, url.length - 1));
+    //Link fatto come https://pokeapi.co/api/v2/pokemon/151/ vado a prenderne id alla fine
+    RegExp regExp = RegExp(r'(\d+)\/$');
+    Match match = regExp.firstMatch(url)!;
+    String numberString = match.group(1)!;
+    int id = int.parse(numberString);
+
     String imageLink =
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png";
 
