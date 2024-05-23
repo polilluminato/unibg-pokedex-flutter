365c4e9ce6ee24ffe4e75144cca9deb463851415
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-05-23 22:19:28 +0200

    drop: Custom UI for the Pokémon list

--- lib/pages/home/ui/card_view.dart
+++ lib/pages/home/ui/card_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:unibg_pokemon/models/pokemon_item.dart';
-import 'package:unibg_pokemon/pages/singlepokemon_page.dart';
 
 class CardView extends StatelessWidget {
   const CardView({super.key, required this.thisPokemon, required this.type});
@@ -12,25 +11,9 @@ class CardView extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData thisTheme = Theme.of(context);
 
-    return InkWell(
-      onTap: () => Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => SinglePokemonPage(
-            pokemonId: thisPokemon.id!,
-          ),
-        ),
-      ),
-      child: Container(
-        child: Row(
-          children: [
-            Text(
-              thisPokemon.name,
-              style: thisTheme.textTheme.titleLarge,
-            )
-          ],
-        ),
-      ),
+    return Text(
+      thisPokemon.name,
+      style: thisTheme.textTheme.titleLarge,
     );
   }
 }
