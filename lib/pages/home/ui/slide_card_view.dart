c642f1d6655fc18bc8701a7563a1e7e047fab967
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-05-25 06:50:19 +0200

    drop: Unused function

--- lib/pages/home/ui/slide_card_view.dart
+++ lib/pages/home/ui/slide_card_view.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:unibg_pokemon/models/pokemon_item.dart';
 import 'package:unibg_pokemon/pages/singlepokemon_page.dart';
-import 'package:unibg_pokemon/utils/string_extensions.dart';
 
 class SlideCardView extends StatelessWidget {
   const SlideCardView({super.key, required this.thisPokemon});
 
   final PokemonItem thisPokemon;
-
-  void doNothing(String actionType) {
-    debugPrint("$actionType) ${thisPokemon.name.capitalize()}");
-  }
 
   @override
   Widget build(BuildContext context) {
