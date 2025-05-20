4bf60bc7869a3f69bfb0bcf3cd36879bfe24db22
Alberto Bonacina <bonacina.alberto@gmail.com>
2025-05-20 22:51:36 +0200

    drop: bottomNavigationBar on HomePage

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:unibg_pokemon/data/models/home_tab_model.dart';
-import 'package:unibg_pokemon/presentation/pages/home/tabs/grid_tab.dart';
 import 'package:unibg_pokemon/presentation/pages/home/tabs/list_tab.dart';
 import 'package:unibg_pokemon/presentation/pages/settings/settings_page.dart';
 import 'package:unibg_pokemon/presentation/styles/dimens.dart';
-
-final indexTabProvider = StateProvider<int>((ref) => 0);
-
-final List<HomeTab> tabList = [
-  HomeTab(
-    label: "List",
-    icon: Icons.list_alt,
-    content: const ListTab(),
-  ),
-  HomeTab(
-    label: "Grid",
-    icon: Icons.grid_view,
-    content: const GridTab(),
-  ),
-];
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -42,21 +25,7 @@ class HomePage extends ConsumerWidget {
           ),
         ],
       ),
-      body: tabList[ref.watch(indexTabProvider)].content,
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          ref.read(indexTabProvider.notifier).state = index;
-        },
-        selectedIndex: ref.watch(indexTabProvider),
-        destinations: tabList
-            .map(
-              (singleScreenTab) => NavigationDestination(
-                icon: Icon(singleScreenTab.icon),
-                label: singleScreenTab.label,
-              ),
-            )
-            .toList(),
-      ),
+      body: ListTab(),
     );
   }
 }
