b12d3f5ddd7ec8907e9f5095266595ed229e2b79
Alberto Bonacina <bonacina.alberto@gmail.com>
2025-05-20 22:52:37 +0200

    drop: Customised Card for Grid and List

--- lib/presentation/pages/home/ui/card_view.dart
+++ lib/presentation/pages/home/ui/card_view.dart
@@ -1,8 +1,6 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:unibg_pokemon/data/models/pokemon_item.dart';
 import 'package:unibg_pokemon/presentation/pages/single_pokemon/singlepokemon_page.dart';
-import 'package:unibg_pokemon/presentation/styles/dimens.dart';
 
 class CardView extends StatelessWidget {
   const CardView({super.key, required this.thisPokemon, required this.type});
@@ -10,30 +8,8 @@ class CardView extends StatelessWidget {
   final PokemonItem thisPokemon;
   final String type;
 
-  List<Widget> _insideContainerList(ThemeData thisTheme) {
-    return [
-      ExtendedImage.network(
-        thisPokemon.imagePng!,
-        width: 72,
-        height: 72,
-      ),
-      const SizedBox(
-        width: Dimens.mainSpace,
-        height: Dimens.smallSpace,
-      ),
-      Expanded(
-        child: Text(
-          thisPokemon.name,
-          style: thisTheme.textTheme.headlineSmall,
-        ),
-      ),
-    ];
-  }
-
   @override
   Widget build(BuildContext context) {
-    ThemeData thisTheme = Theme.of(context);
-
     return InkWell(
       onTap: () => Navigator.push(
         context,
@@ -43,34 +19,7 @@ class CardView extends StatelessWidget {
           ),
         ),
       ),
-      child: Container(
-        margin: EdgeInsets.symmetric(
-          horizontal: type == "list" ? Dimens.smallMargin : Dimens.tinyMargin,
-          vertical: Dimens.tinyMargin,
-        ),
-        padding: const EdgeInsets.symmetric(
-          horizontal: Dimens.mainPadding,
-          vertical: Dimens.smallPadding,
-        ),
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: Dimens.smallBorderSideWidth,
-            color: thisTheme.colorScheme.primary,
-          ),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(Dimens.smallRoundedCorner),
-          ),
-        ),
-        child: switch (type) {
-          "list" => Row(
-              children: _insideContainerList(thisTheme),
-            ),
-          "grid" => Column(
-              children: _insideContainerList(thisTheme),
-            ),
-          _ => throw UnimplementedError(),
-        },
-      ),
+      child: Text(thisPokemon.name),
     );
   }
 }
