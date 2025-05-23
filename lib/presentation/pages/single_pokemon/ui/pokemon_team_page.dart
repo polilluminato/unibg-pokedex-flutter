ac7e5a28ee5f488b0b2b160bf5fa92cdec9ebf78
Alberto Bonacina <bonacina.alberto@gmail.com>
2025-05-23 22:18:43 +0200

    drop: AI team page

--- lib/presentation/pages/single_pokemon/ui/pokemon_team_page.dart
+++ lib/presentation/pages/single_pokemon/ui/pokemon_team_page.dart
@@ -1,10 +1,6 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'package:unibg_pokemon/data/models/ai_pokemon_fight.dart';
 import 'package:unibg_pokemon/config/kprompts.dart';
-import 'package:unibg_pokemon/presentation/pages/single_pokemon/ui/loading_view.dart';
 import 'package:unibg_pokemon/services/ai_service.dart';
-import 'package:unibg_pokemon/utils/utils.dart';
 
 class PokemonTeamPage extends StatefulWidget {
   const PokemonTeamPage({super.key, required this.pokemonName});
@@ -30,34 +26,7 @@ class _PokemonTeamPageState extends State<PokemonTeamPage> {
       appBar: AppBar(
         title: const Text("Team"),
       ),
-      body: FutureBuilder<String?>(
-        future: aiString,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final aiPokemonList = parseAiStringToListMap(snapshot.data!)
-                .map((pokemon) => AiPokemonFight.fromJson(pokemon))
-                .toList();
-
-            return ListView.builder(
-              itemCount: aiPokemonList.length,
-              itemBuilder: (context, index) {
-                final pokemon = aiPokemonList[index];
-                return ListTile(
-                  leading: ExtendedImage.network(
-                    getDefaultPokemonImage(pokemon.id),
-                    height: 72,
-                    width: 72,
-                  ),
-                  title: Text(pokemon.name),
-                  subtitle: Text(pokemon.motivation),
-                );
-              },
-            );
-          } else {
-            return const LoadingView();
-          }
-        },
-      ),
+      body: SizedBox.shrink(),
     );
   }
 }
