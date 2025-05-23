99b213b3b3ffcac6ddb86f12c65e205323c57b33
Alberto Bonacina <bonacina.alberto@gmail.com>
2025-05-23 22:14:29 +0200

    drop: AI opponent page

--- lib/presentation/pages/single_pokemon/ui/pokemon_opponent_page.dart
+++ lib/presentation/pages/single_pokemon/ui/pokemon_opponent_page.dart
@@ -1,13 +1,6 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'package:unibg_pokemon/data/enums/colortype_enum.dart';
-import 'package:unibg_pokemon/data/models/ai_pokemon_fight.dart';
 import 'package:unibg_pokemon/config/kprompts.dart';
-import 'package:unibg_pokemon/presentation/pages/single_pokemon/ui/loading_view.dart';
 import 'package:unibg_pokemon/services/ai_service.dart';
-import 'package:unibg_pokemon/presentation/styles/dimens.dart';
-import 'package:unibg_pokemon/utils/string_extensions.dart';
-import 'package:unibg_pokemon/utils/utils.dart';
 
 class PokemonOpponentPage extends StatefulWidget {
   const PokemonOpponentPage(
@@ -33,63 +26,11 @@ class _PokemonOpponentPageState extends State<PokemonOpponentPage> {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Opponent"),
       ),
-      body: FutureBuilder<String?>(
-        future: aiString,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final aiPokemonFight = AiPokemonFight.fromJson(
-              parseAiStringToMap(snapshot.data!),
-            );
-            return Padding(
-              padding: const EdgeInsets.all(Dimens.mainPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ExtendedImage.network(
-                    getDefaultPokemonImage(aiPokemonFight.id),
-                    width: 200,
-                    height: 200,
-                  ),
-                  Text(aiPokemonFight.name, style: textTheme.headlineMedium),
-                  const SizedBox(height: Dimens.mainSpace),
-                  Wrap(
-                    runSpacing: Dimens.smallPadding,
-                    spacing: Dimens.smallPadding,
-                    children: aiPokemonFight.types
-                        .map(
-                          (type) => Chip(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(
-                                Dimens.hugeRoundedCorner,
-                              ),
-                            ),
-                            label: Text(type.capitalize()),
-                            backgroundColor:
-                                hexToColor(ColorType.fromName(type).color),
-                            labelStyle: const TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                  const SizedBox(height: Dimens.mainSpace),
-                  Text(aiPokemonFight.motivation,
-                      textAlign: TextAlign.start, style: textTheme.bodyLarge),
-                ],
-              ),
-            );
-          } else {
-            return const LoadingView();
-          }
-        },
-      ),
+      body: SizedBox.shrink(),
     );
   }
 }
